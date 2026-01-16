20cd43ada24df091c837d070c17fa6351aa3cf64
ego <ego@nomail.lan>
2026-01-16 14:05:41 +0100

    rem src

--- src/qmemo/db/c_indexes.sql
+++
@@ -1,3 +0,0 @@
-create index if not exists i_memos_deleted
-    on memos (deleted);
-
--- src/qmemo/db/s_oldmemos.sql
+++
@@ -1,5 +0,0 @@
-select *
-from memos
-WHERE (changed IS NOT NULL AND changed > datetime(:tst, 'unixepoch'))
-   OR (changed IS NULL AND created > datetime(:tst, 'unixepoch'))
-order by changed desc, created desc
